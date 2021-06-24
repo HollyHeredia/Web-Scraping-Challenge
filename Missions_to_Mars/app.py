542880ab19a11dcc752d92790baad87d194e35ed
Holly Heredia <hollyheredia@gmail.com>
2021-06-24 16:04:19 -0700

    Adding HTML code and adding style.css

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -13,10 +13,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 def home():
 
     # Find one record of data from the mongo database
-    mars_info = mongo.db.collection.find_one()
+    mars_mongo = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", mars=mars_info)
+    return render_template("index.html", mars=mars_mongo)
 
 
 # Route that will trigger the scrape function
@@ -24,10 +24,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape()
+    mars_info = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    mongo.db.collection.update({}, mars_info, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
