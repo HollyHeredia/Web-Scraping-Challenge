6b631eea6b5a5b87192915f6212f855a65ef332b
Holly Heredia <hollyheredia@gmail.com>
2021-07-01 22:38:10 -0700

    Met with Falconi. Changed the name of some variables. Deleted commented out code.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -13,24 +13,19 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/app")
 def home():
 
     # Find one record of data from the mongo database
-    mars = mongo.db.mars_info.find_one()
+    mars = mongo.db.collection.find_one()
 
     # Return template and data
     return render_template("index.html", mars=mars)
 
 
 # Route that will trigger the scrape function
+
 @app.route("/scrape")
 def scraper():
 
-    # Run the scrape function
-    #mars = mongo.db.mars
-    #mars_data = scrape_mars.scrape()
-    mars = scrape_mars.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    #mars.update({}, mars_data, upsert=True)
-    mongo.db.collection.update({}, mars, upsert=True)
+    mars1 = scrape_mars.scrape_info()
+    mongo.db.collection.update({}, mars1, upsert=True)
 
     # Redirect back to home page
     return redirect("/", code=302)
